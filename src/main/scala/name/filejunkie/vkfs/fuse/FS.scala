a5ad41bd1f516aad22b920c78712bee8d897a898
Ilya Ershov <filejunkie@gmail.com>
2015-10-31 23:41:58 +0100

    Now file is available to download

--- src/main/scala/name/filejunkie/vkfs/fuse/FS.scala
+++ src/main/scala/name/filejunkie/vkfs/fuse/FS.scala
@@ -9,15 +9,21 @@ import ru.serce.jnrfuse.struct.FileStat
 import ru.serce.jnrfuse.struct.FuseFileInfo
 import ru.serce.jnrfuse.ErrorCodes
 import name.filejunkie.vkfs.common.images.Photo
+import jnr.ffi.types.size_t
 
 class FS(userId: String) extends FuseStubFS {
   val vkApi = new VkApi(userId)
   
   override def getattr(path: String, stat: FileStat) : Int = {
     path match {
-      case c if c.endsWith(".jpg") =>{
+      case p if p.endsWith(".jpg") =>{
         stat.st_mode.set(FileStat.S_IFREG | FileStat.S_IRUSR | FileStat.S_IRGRP | FileStat.S_IROTH )
         stat.st_nlink.set(1)
+
+        val photoId = p.substring(p.lastIndexOf("/") + 1, p.length() - 4)
+
+        val bytes = vkApi.getPhoto(photoId)
+        stat.st_size.set(bytes.length)
       }
       case _ => {
         stat.st_mode.set(FileStat.S_IFDIR | FileStat.S_IRUSR | FileStat.S_IXUSR | FileStat.S_IRGRP | FileStat.S_IXGRP | FileStat.S_IROTH | FileStat.S_IXOTH )
@@ -26,7 +32,7 @@ class FS(userId: String) extends FuseStubFS {
     }
     0
   }
-  
+
   override def readdir(path: String, buf: Pointer, filter: FuseFillDir, @off_t offset: Long, fi: FuseFileInfo) : Int = {
     path match {
       case "/" => {
@@ -47,9 +53,40 @@ class FS(userId: String) extends FuseStubFS {
 
             0
           }
-          case _ => ErrorCodes.ENOENT()
+          case _ => -ErrorCodes.ENOENT()
         }
       }
+    }
+  }
+
+  override def open(path: String, fi: FuseFileInfo) = {
+    path match {
+      case p if (p.contains("/") && p.endsWith(".jpg")) => 0
+      case _ => -ErrorCodes.ENOENT()
+    }
+  }
+
+  override def read(path: String, buf: Pointer, @size_t size: Long, @off_t offset: Long, fi: FuseFileInfo) : Int = {
+    path match {
+      case p if (p.contains("/") && p.endsWith(".jpg")) => {
+        val photoId = p.substring(p.lastIndexOf("/") + 1, p.length() - 4)
+
+        val bytes = vkApi.getPhoto(photoId)
+
+        val length = bytes.length
+
+        offset match {
+          case o if o < length => {
+            val s = Math.min(bytes.length - o, size).intValue()
+
+            buf.put(0, bytes, offset.intValue(), s)
+
+            s
+          }
+          case _ => 0
+        }
+      }
+      case _ => -ErrorCodes.ENOENT()
     }
   }
 }
--- src/main/scala/name/filejunkie/vkfs/vk/VkApi.scala
+++ src/main/scala/name/filejunkie/vkfs/vk/VkApi.scala
@@ -4,6 +4,9 @@ import scalaj.http._
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import name.filejunkie.vkfs.common.images._
+import java.net.URL
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 
 class VkApi(userId: String) {
   val apiPrefix = "http://api.vk.com/method/"
@@ -32,5 +35,23 @@ class VkApi(userId: String) {
       }
       case None => None
     }
+  }
+
+  def getPhoto(photoId: String) = {
+    val response: HttpResponse[String] = Http(apiPrefix + "photos.getById").param("owner_id", userId).param("photos",userId + "_" + photoId).param("v","5.37").asString
+
+    val responseString = response.body
+    val json = parse(responseString)
+    val photosJson = json \ "response"
+
+    val photo = photosJson.extract[List[Photo]].last
+
+    val urlStr = photo.photo_2560.getOrElse(photo.photo_1280.getOrElse(photo.photo_807.getOrElse(photo.photo_604.get)))
+
+    val url = new URL(urlStr)
+
+    val is = new BufferedInputStream(url.openStream())
+
+    Stream.continually(is.read).takeWhile(_ != -1).map(_.toByte).toArray
   }
 }
